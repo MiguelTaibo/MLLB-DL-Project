2a095cada8fb2d174ba89e7653e714fc63be2f54
MiguelTaibo <m.taibo@alumnos.upm.es>
2021-12-28 11:06:40 +0100

    first commit

---
+++ models/vgg16.py
@@ -0,0 +1,38 @@
+from keras.layers import Conv2D, Dropout, Flatten, MaxPooling2D, Input, Dense
+from keras.utils.version_utils import ModelVersionSelector
+from keras.constraints import maxnorm
+
+def VGG16(dropout, num_classes=10, img_width=32, img_height=32, img_channels=3):
+    input_image = Input(shape=(img_width,img_height,img_channels))
+    x1 = Conv2D(64, (3, 3),padding='same', activation='relu')(input_image)
+    x1 = Conv2D(64, (3, 3),padding='same', activation='relu')(x1)
+    x1 = MaxPooling2D((2, 2))(x1)
+
+    x2 = Conv2D(128, (3, 3),padding='same', activation='relu')(x1)
+    x2 = Conv2D(128, (3, 3),padding='same', activation='relu')(x2)
+    x2 = MaxPooling2D((2, 2))(x2)
+
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x2)
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x3)
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x3)
+    x3 = MaxPooling2D((2, 2))(x3)
+
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x3)
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
+    x4 = MaxPooling2D((2, 2))(x4)
+
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x5)
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x5)
+    x5 = MaxPooling2D((2, 2))(x5)
+
+    x6 = Flatten()(x5)
+    x6=Dropout(dropout)(x6)
+    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3))(x6)
+    x6=Dropout(dropout)(x6)
+    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3))(x6)
+    x=Dropout(dropout)(x6)
+    out= Dense(num_classes, activation='softmax')(x)
+
+    model = ModelVersionSelector(inputs = input_image, outputs = out)
