1b5f1257aba54a5f89d896dfc7e3e8d364d0c67f
fonsms <alfonsomsanz@alumnos.upm.es>
2022-01-05 17:09:26 +0100

    Regulation

--- models.py
+++ models.py
@@ -1,37 +1,77 @@
 from keras.layers import Conv2D, Dropout, Flatten, MaxPooling2D, Input, Dense, InputLayer
 from keras.constraints import maxnorm
-from keras.models import Model,Sequential
-
-def VGG16(dropout, num_classes=10, img_width=32, img_height=32, img_channels=3):
+from keras.models import Model
+from keras.regularizers import l2
+def VGG16(dropout, num_classes=10, img_width=32, img_height=32, img_channels=3,l2_reg=0):
     input_image = Input(shape=(img_width,img_height,img_channels))
-    x1 = Conv2D(64, (3, 3),padding='same', activation='relu')(input_image)
-    x1 = Conv2D(64, (3, 3),padding='same', activation='relu')(x1)
+    x1 = Conv2D(64, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(input_image)
+    x1 = Conv2D(64, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x1)
     x1 = MaxPooling2D((2, 2))(x1)
 
-    x2 = Conv2D(128, (3, 3),padding='same', activation='relu')(x1)
-    x2 = Conv2D(128, (3, 3),padding='same', activation='relu')(x2)
+    x2 = Conv2D(128, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x1)
+    x2 = Conv2D(128, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x2)
     x2 = MaxPooling2D((2, 2))(x2)
 
-    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x2)
-    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x3)
-    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x3)
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x2)
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x3)
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x3)
     x3 = MaxPooling2D((2, 2))(x3)
 
-    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x3)
-    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
-    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x3)
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x4)
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x4)
     x4 = MaxPooling2D((2, 2))(x4)
 
-    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
-    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x5)
-    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x5)
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x4)
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x5)
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x5)
     x5 = MaxPooling2D((2, 2))(x5)
 
     x6 = Flatten()(x5)
     x6=Dropout(dropout)(x6)
-    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3))(x6)
+    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3),kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x6)
     x6=Dropout(dropout)(x6)
-    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3))(x6)
+    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3),kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x6)
+    x=Dropout(dropout)(x6)
+    out= Dense(num_classes, activation='softmax',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x)
+
+    model = Model(inputs = input_image, outputs = out)
+    
+    return model
+
+def VGG19(dropout, num_classes=10, img_width=32, img_height=32, img_channels=3,l2_reg=0):
+    input_image = Input(shape=(img_width,img_height,img_channels))
+    x1 = Conv2D(64, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(input_image)
+    x1 = Conv2D(64, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x1)
+    x1 = MaxPooling2D((2, 2))(x1)
+
+    x2 = Conv2D(128, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x1)
+    x2 = Conv2D(128, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x2)
+    x2 = MaxPooling2D((2, 2))(x2)
+
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x2)
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x3)
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x3)
+    x3 = Conv2D(256, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x3)
+    x3 = MaxPooling2D((2, 2))(x3)
+
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x3)
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x4)
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x4)
+    x4 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x4)
+    x4 = MaxPooling2D((2, 2))(x4)
+
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x4)
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x5)
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x5)
+    x5 = Conv2D(512, (3, 3),padding='same', activation='relu',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x5)
+    x5 = MaxPooling2D((2, 2))(x5)
+
+    x6 = Flatten()(x5)
+    x6=Dropout(dropout)(x6)
+    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3),kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x6)
+    x6=Dropout(dropout)(x6)
+    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3),kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x6)
     x=Dropout(dropout)(x6)
     out= Dense(num_classes, activation='softmax')(x)
 
@@ -39,53 +79,13 @@ def VGG16(dropout, num_classes=10, img_width=32, img_height=32, img_channels=3):
     
     return model
 
-def VGG19(dropout, num_classes=10, img_width=32, img_height=32, img_channels=3):
-    input_image = Input(shape=(img_width,img_height,img_channels))
-    x1 = Conv2D(64, (3, 3),padding='same', activation='relu')(input_image)
-    x1 = Conv2D(64, (3, 3),padding='same', activation='relu')(x1)
-    x1 = MaxPooling2D((2, 2))(x1)
-
-    x2 = Conv2D(128, (3, 3),padding='same', activation='relu')(x1)
-    x2 = Conv2D(128, (3, 3),padding='same', activation='relu')(x2)
-    x2 = MaxPooling2D((2, 2))(x2)
-
-    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x2)
-    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x3)
-    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x3)
-    x3 = Conv2D(256, (3, 3),padding='same', activation='relu')(x3)
-    x3 = MaxPooling2D((2, 2))(x3)
-
-    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x3)
-    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
-    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
-    x4 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
-    x4 = MaxPooling2D((2, 2))(x4)
-
-    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x4)
-    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x5)
-    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x5)
-    x5 = Conv2D(512, (3, 3),padding='same', activation='relu')(x5)
-    x5 = MaxPooling2D((2, 2))(x5)
-
-    x6 = Flatten()(x5)
-    x6=Dropout(dropout)(x6)
-    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3))(x6)
-    x6=Dropout(dropout)(x6)
-    x6=Dense(512, activation='relu', kernel_constraint=maxnorm(3))(x6)
-    x=Dropout(dropout)(x6)
-    out= Dense(num_classes, activation='softmax')(x)
-
-    model = Model(inputs = input_image, outputs = out)
-    
-    return model
-
-def MLP_model(n_hidden, img_width=32, img_height=32, img_channels=3, num_classes=10):
+def MLP_model(n_hidden, img_width=32, img_height=32, img_channels=3, num_classes=10,l2_reg=0):
     input_image = Input(shape=(img_width,img_height,img_channels))
     x = Flatten()(input_image)
     
     for layer in reversed(range(n_hidden)):
-        x = Dense((img_height*img_width+num_classes)*(layer+1)/(n_hidden+1), activation='relu', kernel_constraint=maxnorm(3))(x)
-    out= Dense(num_classes, activation='softmax')(x)
+        x = Dense(int((img_height*img_width+num_classes)*(layer+1)/(n_hidden+1)), activation='relu', kernel_constraint=maxnorm(3),kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x)
+    out= Dense(num_classes, activation='softmax',kernel_initializer="glorot_uniform",kernel_regularizer=l2(l2_reg),bias_regularizer=l2(l2_reg))(x)
     model = Model(inputs = input_image, outputs = out)
     
     return model
